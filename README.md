a7bb6840e481928cd9e9b53cb560b134190bcaf7
DarkJediVader <31324137+DarkJediVader@users.noreply.github.com>
2020-04-27 18:43:48 -0600

    argument parsing added

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # 6Degrees
 Given an artist name, create a new playlist consisting of their and related artists
 top songs.
+
+- .sid file
+  - First line must be Client ID
+  - Second line must be Client Secret
--- Source/6Degrees.py
+++ Source/6Degrees.py
@@ -1,11 +1,51 @@
+import sys
+from optparse import OptionParser
 
+client_id = None
+client_secret = None
+degrees = 1
+playlist_name = '{0}Degrees of {1}'
 
+"""
+Parse args for the program. First must be sid filename, remaining are optional.
+"""
+def parse_args(args):
+    usage = 'usage: {0} <sid_file> [options]'
+    if len(args) > 1:
+        filename = args[1]
 
+        parser = OptionParser(usage=usage.format('%prog'))
+        parser.add_option('-d', '--Degrees', default=1, dest='degrees', type='int', help='Set how many layers deep the application goes. The higher the number the longer the runtime and larger the playlist.')
 
+        (options, args) = parser.parse_args()
 
+        global degrees
+        degrees = options.degrees
 
+        parse_sid(filename)
+    else:
+        print (usage.format(args[0]))
+        sys.exit()
 
+"""
+Set the client id and client secret found in the file specified by filename
+"""
+def parse_sid(filename):
+    if filename.endswith('.sid'):
+        try:
+            sid = open(filename, 'r')
 
+            global client_id
+            client_id = sid.readline().strip()
+
+            global client_secret
+            client_secret = sid.readline().strip()
+        except:
+            print ('Problem reading file \'{0}\''.format(filename))
+            sys.exit()
+    else:
+        print ('Invalid file format. File must be of type \'.sid\'')
+        sys.exit()
 
 if __name__ == '__main__':
-    pass
+    parse_args(sys.argv)
